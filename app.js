126941f558cfbf171fd645ee745b0180eac35ff8
Samaneh-Dehghanpour <120442130+Samaneh-Dehghanpor@users.noreply.github.com>
2023-01-05 20:38:59 +0330

    Cleaning up the code

--- app.js
+++ app.js
@@ -7,10 +7,8 @@ function formatDay(timestamp) {
 }
 function displayForecast(result) {
   let forecast = result.data.daily;
-  console.log(result);
-
+  let icon = result.data.daily[0].condition.icon_url;
   let forecastElement = document.querySelector("#forecast");
-
   let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
     if (index < 6) {
@@ -20,9 +18,10 @@ function displayForecast(result) {
       <div class="col-2">
         <div class="forecast-date">${formatDay(forecastDay.time)}</div>
         <img
-          src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/few-clouds-day.png"
+          src="${icon}"
           alt=""
-          width="42"
+          width="60"
+          class="forecast-img"
         />
         <div class="forecast-number">
           <span class="max"> ${Math.round(
@@ -42,21 +41,25 @@ function displayForecast(result) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
+  let lat = coordinates.latitude;
+  let lon = coordinates.longitude;
   let apiKey = "f8353f340ebct4369a4b13o590322fed";
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lat=38.71667&lon=-9.13333&key=f8353f340ebct4369a4b13o590322fed&units=metric`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lat=${lat}&lon=${lon}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
+
 function handleSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
   search(searchInput.value);
 }
+
 function search(city) {
   let apiKey = "f8353f340ebct4369a4b13o590322fed";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showCity);
 }
+
 function showCity(result) {
   console.log(result.data);
   let cityName = document.querySelector("#city-name");
@@ -68,6 +71,11 @@ function showCity(result) {
   let currentDate = document.querySelector("#current-date");
   let date = new Date(result.data.time * 1000);
   let day = date.getDate();
+  let month = monthsList[date.getMonth()];
+  let year = date.getFullYear();
+  let hour = ("0" + date.getHours()).substr(-2);
+  let minute = ("0" + date.getMinutes()).substr(-2);
+  celciusTemp = Math.round(result.data.temperature.current);
   let monthsList = [
     "January",
     "February",
@@ -82,12 +90,6 @@ function showCity(result) {
     "November",
     "December",
   ];
-  let month = monthsList[date.getMonth()];
-  let year = date.getFullYear();
-  let hour = ("0" + date.getHours()).substr(-2);
-  let minute = ("0" + date.getMinutes()).substr(-2);
-
-  celciusTemp = Math.round(result.data.temperature.current);
 
   cityName.innerHTML = result.data.city;
   currentTemp.innerHTML = Math.round(result.data.temperature.current);
@@ -101,6 +103,7 @@ function showCity(result) {
   currentDate.innerHTML = `${day} ${month} ${year}, ${hour}:${minute}`;
   getForecast(result.data.coordinates);
 }
+
 function displayFahreniet(event) {
   event.preventDefault();
   celcius.classList.remove("active");
@@ -109,6 +112,7 @@ function displayFahreniet(event) {
   let fahreneitTemp = (celciusTemp * 9) / 5 + 32;
   currentTemp.innerHTML = Math.round(fahreneitTemp);
 }
+
 function displayCelcius(event) {
   event.preventDefault();
   celcius.classList.add("active");
@@ -120,11 +124,8 @@ function displayCelcius(event) {
 let celciusTemp = null;
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
-
 let fahreneit = document.querySelector("#fahreneit");
 fahreneit.addEventListener("click", displayFahreniet);
-
 let celcius = document.querySelector("#celcius");
 celcius.addEventListener("click", displayCelcius);
-
 search("Lisbon");
